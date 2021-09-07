8cf1c7eda8cf8336339fe7bb7af574779b19aa6d
yshin8910 <69772219+yshin8910@users.noreply.github.com>
2021-09-07 12:51:11 -0400

    Add files via upload
    stopped at 2:13:21
    Everything is working except peer-peer connection (video 1:21:00, line 36 in server.js) due to "Cannot read property 'emit' of undefined" error.

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express(); //Initialize express application
+//create server
+const server = require('http').Server(app);
+//import socket.io
+const io = require('socket.io')(server)
+//import uuid to create an unique room id
+const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+	debug: true
+});
+
+//set view file as ejs
+app.set('view engine', 'ejs');
+
+//static folder inside the public url
+app.use(express.static('public'));
+
+
+app.use('/peerjs', peerServer);
+app.get('/', (req, res) => {
+	res.redirect(`/${uuidv4()}`);
+})
+
+
+app.get('/:room', (req, res) => {
+	res.render('room', { roomId: req.params.room });
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    //Cannot read property 'emit' of undefined
+    //video 1:21:00
+    //socket.to(roomId).broadcast.emit('user-connected', userId);
+  }); 
+})
+
+server.listen(3030);
